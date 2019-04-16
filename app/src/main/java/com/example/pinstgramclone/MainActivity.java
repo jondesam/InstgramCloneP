b46ddee9a30f4012f18b18acb088e148271dc2df
Jonghyun <godaiys@hotmail.com>
2019-04-16 17:37:34 -0400

    Intput from user

--- app/src/main/java/com/example/pinstgramclone/MainActivity.java
+++ app/src/main/java/com/example/pinstgramclone/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.pinstgramclone;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.parse.ParseException;
@@ -14,34 +16,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 //import java.lang.annotation.Target;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnSave;
+    private EditText editName;
+    private EditText editSpeed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnSave = findViewById(R.id.btnSave);
+        editName = findViewById(R.id.editName);
+        editSpeed = findViewById(R.id.speed);
+        btnSave.setOnClickListener(MainActivity.this);
+
+
 
     }
 
-    public  void helloWorldTapped(View view) {
 
-        ParseObject player = new ParseObject("Player");
-        player.put("name", "jon");
-        player.put("speed", "10");
-        player.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-//                    Toast.makeText(MainActivity.this, "saved", Toast.LENGTH_SHORT).show();
-//                    FancyToast.makeText(this,"Hello World !",FancyToast.LENGTH_LONG,FancyToast.DEFAULT,true);
-                    FancyToast.makeText(MainActivity.this, "it's fancily saved", Toast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+    @Override
+    public void onClick(View v) {
+
+
+            ParseObject player = new ParseObject("Player");
+            player.put("name", editName.getText().toString());
+            player.put("speed",  editSpeed.getText().toString());
+
+            player.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+//
+                        FancyToast.makeText(MainActivity.this, "it's fancily saved", Toast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
                 }
-            }
-        });
+            });
+
+
 
     }
-
-
 }
 
