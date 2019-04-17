fd2f352b85cbb403c6680dd2cf87dfe98ac0e7d2
Jonghyun <godaiys@hotmail.com>
2019-04-17 15:06:05 -0400

    Get data from parse server

--- app/src/main/java/com/example/pinstgramclone/MainActivity.java
+++ app/src/main/java/com/example/pinstgramclone/MainActivity.java
@@ -7,12 +7,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
+import java.util.jar.Attributes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,42 +29,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText editSpeed;
     private EditText textGetData;
     private EditText editPower;
+    private Button btnAllPlayer;
+    private String allPlayers;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        btnSave = findViewById(R.id.btnSave);
-        editName = findViewById(R.id.editName);
-        editSpeed = findViewById(R.id.speed);
-        btnSave.setOnClickListener(MainActivity.this);
-        textGetData = findViewById(R.id.textGetData);
-        editPower = findViewById(R.id.power);
+            btnSave = findViewById(R.id.btnSave);
+            editName = findViewById(R.id.editName);
+            editSpeed = findViewById(R.id.speed);
+            btnSave.setOnClickListener(MainActivity.this);
+            textGetData = findViewById(R.id.textGetData);
+            editPower = findViewById(R.id.power);
+            btnAllPlayer = findViewById(R.id.btnAllPlayer);
 
 
-        //query from server
-        textGetData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Player");
-                parseQuery.getInBackground("0BSyhI5cPd", new GetCallback<ParseObject>() {
-                    @Override
-                    public void done(ParseObject object, ParseException e) {
-                        if (object != null && e == null) {
+            //query from server using getInBackground
+            textGetData.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-                            textGetData.setText(object.get("Speed") + "");
-                        } else {
-                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Player");
+
+                    parseQuery.getInBackground("9BwPB3cbvf", new GetCallback<ParseObject>() {
+                        @Override
+                        public void done(ParseObject object, ParseException e) {
+                            if (object != null && e == null) {
+
+                                textGetData.setText(" name = " + object.get("name") + "" +", " + "power = "+
+                                        object.get("power") + "" + ", " + "speed = " + object.get("speed"));
+                            } else {
+                                Toast.makeText(MainActivity.this, e.getMessage(),
+                                        Toast.LENGTH_LONG).show();
+                            }
+
                         }
+                    });
 
-                    }
-                });
+                }
+            });
 
-            }
-        });
+             //query from server using findInBackground
+            btnAllPlayer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    allPlayers = "";
+                    ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Player");
 
+                    parseQuery.findInBackground(new FindCallback<ParseObject>() {
+                        @Override
+                        public void done(List<ParseObject> objects, ParseException e) {
+                            if (e == null) {
+                                if (objects.size() > 0) {
+                                    for (ParseObject player: objects) {
+                                        allPlayers = allPlayers + player.get("name") + "\n";
+                                    }
+
+                                    FancyToast.makeText(MainActivity.this, allPlayers,
+                                            Toast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                                } else {
+
+                                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+                    });
+
+                }
+            });
 
 
     }
@@ -73,14 +113,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ParseObject player = new ParseObject("Player");
             player.put("name", editName.getText().toString());
             player.put("speed", editSpeed.getText().toString());
-            player.put("power", Integer.parseInt( editSpeed.getText().toString()));
+            player.put("power", Integer.parseInt( editPower.getText().toString()));
 
             player.saveInBackground(new SaveCallback() {
                 @Override
                 public void done(ParseException e) {
                     if (e == null) {
 //
-                        FancyToast.makeText(MainActivity.this, "it's fancily saved", Toast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                        FancyToast.makeText(MainActivity.this, "it's fancily saved",
+                                Toast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
                     } else {
                         Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
                     }
