86cf01aebe661e2a6f9ee9436e4b1cb8e4667eb8
Jonghyun <godaiys@hotmail.com>
2019-04-18 14:09:48 -0400

    welcome activity parsing user name

--- app/src/main/java/com/example/pinstgramclone/SignUpActivity.java
+++ app/src/main/java/com/example/pinstgramclone/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.example.pinstgramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -48,6 +49,10 @@ public class SignUpActivity extends AppCompatActivity {
                             FancyToast.makeText(SignUpActivity.this,
                                     appUser.get("username") + " has signed up!",
                                     Toast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+
+                            Intent intent = new Intent(SignUpActivity.this, WelcomeActivity.class);
+                            startActivity(intent);
+
                         } else {
                             FancyToast.makeText(SignUpActivity.this,
                                     FancyToast.ERROR, Toast.LENGTH_LONG).show();
@@ -71,6 +76,9 @@ public class SignUpActivity extends AppCompatActivity {
                                      FancyToast.makeText(SignUpActivity.this,
                                              user.get("username") + " has logged in!",
                                              Toast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+
+                                     Intent intent = new Intent(SignUpActivity.this, WelcomeActivity.class);
+                                     startActivity(intent);
                                  } else {
                                      Toast.makeText(SignUpActivity.this,
                                              e.getMessage(),
